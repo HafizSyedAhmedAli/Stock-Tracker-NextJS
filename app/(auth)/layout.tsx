aecf566d5ee0b38f99bf51460e5e90b3a4a7a1f3
HafizSyedAhmedAli <hafizsyedahmedali12@gmail.com>
2025-12-07 22:03:56 +0500

    Implemented auth logic

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,15 @@
 import Link from "next/link";
 import Image from "next/image";
+import {auth} from "@/lib/better-auth/auth";
+import {headers} from "next/headers";
+import {redirect} from "next/navigation";
 
-const Layout = ({children}: { children: React.ReactNode }) => {
+export default async function Layout({children}: { children: React.ReactNode }) {
+
+    const session = await auth.api.getSession({headers: await headers()});
+
+    if (session?.user) redirect("/");
+
     return (
         <main className="auth-layout">
             <section className="auth-left-section scrollbar-hide-default">
@@ -34,10 +42,10 @@ const Layout = ({children}: { children: React.ReactNode }) => {
                 </div>
 
                 <div className="flex-1 relative">
-                    <Image src="/assets/images/dashboard.png" alt="Dashboard Preview" width={1440} height={1150} className="auth-dashboard-preview absolute" />
+                    <Image src="/assets/images/dashboard.png" alt="Dashboard Preview" width={1440} height={1150}
+                           className="auth-dashboard-preview absolute"/>
                 </div>
             </section>
         </main>
     )
 }
-export default Layout
